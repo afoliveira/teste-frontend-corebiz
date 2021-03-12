ece4afe1c2dd25244f2695c79bf47b93d11643ff
Afonso <afonso.noliveira29@gmail.com>
2021-03-12 11:57:21 -0300

    Fix routes to get products

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://corebiz-test.herokuapp.com/api/v1/'
+  baseURL: 'https://corebiz-test.herokuapp.com/api/v1'
 })
 
 export const baseFetch = async () => {
   return api
-    .get(products)
+    .get('/products')
     .then((res) => res.data)
     .catch((err) => console.log(err))
 }
