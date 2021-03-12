26a1e9fae49c99cb35cce7ea4db336b8c8689727
Afonso <afonso.noliveira29@gmail.com>
2021-03-12 11:52:03 -0300

    Fix header responsiveness

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import {React} from 'react';
-import {Container, Logo, ContainerInput, ContainerUser} from './styles';
+import {Container, Logo, ContainerInput, ContainerUser, MenuHamburguer} from './styles';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from '../../assets/logo.svg'
+import logoResponsivo from '../../assets/logo-responsiva.svg'
 import search from '../../assets/icons/search.svg'
 import user from '../../assets/icons/user.svg'
 import cart from '../../assets/icons/cart.svg'
@@ -9,13 +11,17 @@ import cart from '../../assets/icons/cart.svg'
 export default function Header () {
   return (
     <Container>
-      <Logo src={logo}/>
+      <Logo src={window.innerWidth > 800 ? logo : logoResponsivo}/>
+      <MenuHamburguer>
+        <GiHamburgerMenu />
+      </MenuHamburguer>
       <ContainerInput>
-        <input 
+        <input
           className='input__field'
           type= 'text'
           defaultValue= ''
           placeholder= 'O que está procurando?'
+          id='search'
         />
         <img className='input__icon' src={search}/>
       </ContainerInput>
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -2,17 +2,37 @@ import styled from 'styled-components'
 
 export const Container = styled.header`
   background-color: #fff;
-  height: 80px;
+  height: 90px;
   max-width: 1220px;
   margin: 0 auto;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
-`; 
+
+  @media(max-width: 800px) {
+    padding: 5px 20px;
+  }
+`;
+
+export const MenuHamburguer = styled.div`
+  display: none;
+
+  @media(max-width: 800px) {
+    display: block;
+    order: -1;
+  }
+
+`;
 
 export const Logo = styled.img`
   max-width: 170.14px;
   max-height: 41px;
+
+  @media(max-width: 800px) {
+    margin-bottom: 16px;
+    order: 0;
+  }
 `;
 
 export const ContainerInput = styled.div`
@@ -25,6 +45,7 @@ export const ContainerInput = styled.div`
     border: none;
     border-bottom: 1px solid #7A7A7A;
     outline: none;
+    padding: 5px 0;
   }
 
   & .input__icon {
@@ -32,6 +53,10 @@ export const ContainerInput = styled.div`
     right: 0;
     bottom: 5px;
     padding: 2px 4px;
+  }
+
+  @media(max-width: 800px) {
+    order: 2;
   }
 `;
 
@@ -48,6 +73,15 @@ export const ContainerUser = styled.div`
       color: #7a7a7a;
       font-size: 13px;
       line-height: 17.73px;
+    }
+  }
+
+  @media(max-width: 800px) {
+    .account {
+      display: none;
+    }
+    .cart {
+      order: 1;
     }
   }
 `;
